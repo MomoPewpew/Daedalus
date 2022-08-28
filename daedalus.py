92cbfb45c07e08118ce2a012db35e0c391c9965d
marijntepas <marijntepas@gmail.com>
2022-08-28 18:31:39 +0200

    Rewrote daedalus main to use parameters

--- daedalus.py
+++ daedalus.py
@@ -1,3 +1,56 @@
-from plugins.ArcaneGAN import arcanegan
+import argparse
+import asyncio
+import os
 
-arcanegan.process()
+taskID = -1
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--taskID",
+        type=int,
+        default="-1",
+        help="The ID of the task that is being worked on."
+    )
+    parser.add_argument(
+        "--function",
+        type=str,
+        default="",
+        help="What function we are using"
+    )
+    parser.add_argument(
+        "--sourceURL",
+        type=str,
+        default="",
+        help="The URL to the source that we need to download."
+    )
+    parser.add_argument(
+        "--args",
+        type=str,
+        default="",
+        help="The arguments that will be sent to the called function"
+    )
+
+    opt = parser.parse_args()
+
+    if opt.function == "":
+        return
+
+    if opt.sourceURL != "":
+        pass
+        ##TODO: Download source url
+
+    taskID = opt.taskID
+
+    eval(opt.function + '(opt.args)')
+
+def arcanegan(args: str) -> None:
+    cmd = 'conda run -n arcanegan python3 Daedalus/plugins/ArcaneGAN/arcanegan.py'
+    handlecmd(cmd)
+
+def handlecmd(cmd: str) -> None:
+    os.system(cmd)
+
+if __name__ == "__main__":
+    main()
--- plugins/ArcaneGAN/arcanegan.py
+++ plugins/ArcaneGAN/arcanegan.py
@@ -112,7 +112,7 @@ def proc_pil_img(input_image, model):
 version = '0.4' #@param ['0.1','0.2','0.3','0.4']
 
 model_path = os.path.join(os.path.dirname(__file__) + f'/content/ArcaneGANv{version}.jit')
-in_dir = os.path.join(os.path.dirname(__file__) + '/content/in')
+in_dir = os.path.join('Daedalus/in')
 out_dir = os.path.join('Daedalus/out')
 model = torch.jit.load(model_path).eval().cuda().half()
 
@@ -177,3 +177,6 @@ def process(upload=True):
   processed = sorted(glob(f'{out_dir}/*'))[:3]
 #  for f in processed: 
 #    show_img(f, 256)
+
+if __name__ == "__main__":
+    process()
