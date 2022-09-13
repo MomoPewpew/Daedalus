6d04321b97ef4cbdabbfc098b39fb75d7de3d4eb
marijntepas <marijntepas@gmail.com>
2022-09-13 18:16:28 +0200

    Added conda env for SD

--- plugins/stable-diffusion/scripts/img2img.py
+++ plugins/stable-diffusion/scripts/img2img.py
@@ -176,7 +176,7 @@ def main():
     parser.add_argument(
         "--ckpt",
         type=str,
-        default="models/ldm/stable-diffusion-v1/model.ckpt",
+        default="models/ldm/stable-diffusion-v1/sd-v1-4.ckpt",
         help="path to checkpoint of model",
     )
     parser.add_argument(
